a99aa9e47ccd2b6e2c0b87723481997f379ccab9
Александр Пятин <pyatin.aa1242@gmail.com>
2023-02-15 21:02:08 +0500

    test

--- pz_2/Program.cs
+++ pz_2/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Drawing;
+using System.Numerics;
+using System.Runtime;
 
 namespace pz_2
 {
@@ -6,7 +9,43 @@ namespace pz_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+               bool[,] M = new bool[5, 5]
+               {
+                  {false, true, true, false, false}, 
+                  {false, false, false, true, false},
+                  {false, true, false, false, false},
+                  {false, false, true, false, false},
+                  {false, false, false, true, false}
+               };
+               Graph graph = new Graph(5, M);
+               graph.Depth(1);
         }
     }
+        public class Graph
+        {
+            private int size; 
+            private bool[,] adjacency; 
+            private bool[] vector; 
+            public int Size { get; set; }
+            public bool[,] Adjacency { get; set; }
+            public bool[] Vector { get; set; }
+            public Graph(int size, bool[,] G) 
+            {
+                Adjacency = new bool[size, size]; 
+                Adjacency = G;
+                Vector = new bool[size];
+                for (int i = 0; i < size; i++)
+                    Vector[i] = false; 
+                Size = size;
+            }
+            public void Depth(int i) 
+            {
+                Vector[i] = true; 
+                Console.Write("{0}" + ' ', i); 
+                for (int k = 0; k < Size; k++) 
+                     if (Adjacency[i, k] && !(Vector[k]))
+                    Depth(k); 
+            }
+        }
+    
 }
