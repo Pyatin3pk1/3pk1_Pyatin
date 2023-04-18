f41b08d1ebacb7d33df269020c22ef1cc39ff6b5
Pyatin <saschs.puatin@gmail.com>
2023-04-18 16:43:05 +0500

    test

--- pz_2/Program.cs
+++ pz_2/Program.cs
@@ -18,35 +18,7 @@ namespace pz_2
                {0, 0, 1, 0, 0},
                {0, 0, 0, 1, 0}
             };
-            for (int i = 0; i < n; i++)
-            {
-                for (int j = 0; j < n; j++)
-                {
-                    if (i == j) { a[i, j] = 1; }
-
-                    if (a[i, j] != 1)
-                    {
-                        for (int k = 0; k < n; k++)
-                        {
-                            if (j != 0)
-                            {
-                                if (a[j, j - 1] == 1)
-                                {
-                                    a[i, j] = 1;
-                                }
-                                else
-                                {
-                                    if (a[k, j] == 1) a[k, j] = 1;
-                                    else
-                                    {
-                                        if (a[k, i - 1] == 1) a[i, j] = 1;
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
+            
 
             for (int k = 0; k < n; k++)
             {
