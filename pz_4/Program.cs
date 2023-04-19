5ddb08635912afbf0c96522aa6d840edcf54286f
Александр Пятин <pyatin.aa1242@gmail.com>
2023-04-20 00:07:37 +0500

    test

---
+++ pz_4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace pz_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SearchTree st = new SearchTree();
+            Random random = new Random();
+            for (int i = 1; i < 10; i++)
+            {
+                int value = random.Next(10, 1001);
+                st.Add(value);
+                Console.WriteLine("Значение узла дерева = {1}", i, value);
+            }
+            int s = st.Sum();
+            Console.WriteLine($"Сумма значений информационных полей: {s}");
+            int c = st.NumberInternalNodes();
+            Console.WriteLine($"Количество внутренних узлов: {c}");
+        }
+    }
+}
---
+++ pz_4/SearchTree.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_4
+{
+    class SearchTree
+    {
+        public Node root;
+        public void Add(int value)
+        {
+            root = CreatingTree(root, value);
+        }
+        public Node CreatingTree(Node root, int value)
+        {
+            if (root == null)
+            {
+                root = new Node(value);
+                return root;
+            }
+            if (value < root.value)
+            {
+               root.left = CreatingTree(root.left, value);
+            } 
+            else if (value > root.value)
+            {
+               root.right = CreatingTree(root.right, value);
+            } 
+            return root;
+        }
+        public int Sum()
+        {
+            return SumTr(root);
+        }
+        private int SumTr(Node root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            return root.value + SumTr(root.left) + SumTr(root.right);
+        }
+        public int NumberInternalNodes()
+        {
+            return CountingNumberInternalNodes(root);
+        }
+        public static int CountingNumberInternalNodes(Node root)
+        {
+            if (root == null || (root.left == null && root.right == null))
+            {
+                return 0;
+            }
+
+            return 1 + CountingNumberInternalNodes(root.left) + CountingNumberInternalNodes(root.right);
+        }
+    }
+}
